9daa57e0f9d62692db4a974ac0a9573f4a2d31fd
oussema sahbeni <oussemasahbeni300@gmail.com>
2025-12-01 00:14:58 +0100

    feat: enhance prerendering by reintroducing blog post transformation logic

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 /// <reference types="vitest" />
 
-import analog from '@analogjs/platform';
+import analog, { PrerenderContentFile } from '@analogjs/platform';
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
 import viteTsConfigPaths from 'vite-tsconfig-paths';
-import { getBlogPosts } from './vite-prerender.utils';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
@@ -26,19 +25,18 @@ export default defineConfig(({ mode }) => {
           routes: [
             '/',
             '/blog',
-            ...getBlogPosts(),
-            // {
-            //   contentDir: 'src/content/blog',
-            //   transform: (file: PrerenderContentFile) => {
-            //     // do not include files marked as draft in frontmatter
-            //     if (file.attributes['draft']) {
-            //       return false;
-            //     }
-            //     // use the slug from frontmatter if defined, otherwise use the files basename
-            //     const slug = file.attributes['slug'] || file.name;
-            //     return `/blog/${slug}`;
-            //   },
-            // },
+            {
+              contentDir: 'src/content/blog',
+              transform: (file: PrerenderContentFile) => {
+                // do not include files marked as draft in frontmatter
+                if (file.attributes['draft']) {
+                  return false;
+                }
+                // use the slug from frontmatter if defined, otherwise use the files basename
+                const slug = file.attributes['slug'] || file.name;
+                return `/blog/${slug}`;
+              },
+            },
           ],
         },
       }),
